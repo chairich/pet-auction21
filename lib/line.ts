810a44a1152401858edf0234cc1dfca2565a1643
chairich <33069090+chairich@users.noreply.github.com>
2025-07-01 10:42:08 +0700

    Update line.ts

--- lib/line.ts
+++ lib/line.ts
@@ -1,11 +1,12 @@
-export async function sendLineNotify(message: string) {
-  const token = process.env.LINE_NOTIFY_TOKEN!;
-  await fetch("https://notify-api.line.me/api/notify", {
+export async function sendLineNotify(message: string, token: string) {
+  const res = await fetch("https://notify-api.line.me/api/notify", {
     method: "POST",
     headers: {
       "Content-Type": "application/x-www-form-urlencoded",
-      Authorization: \`Bearer \${token}\`,
+      Authorization: `Bearer ${token}`,
     },
-    body: new URLSearchParams({ message }),
+    body: new URLSearchParams({ message }).toString(),
   });
+
+  return res.ok;
 }
